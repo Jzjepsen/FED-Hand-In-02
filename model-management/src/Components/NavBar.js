88f491066ca288f778f7b3c1df4500e24971d388
Heidi <202106684@post.au.dk>
2023-11-15 11:48:08 +0100

    Adding navigation

---
+++ model-management/src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom";
+
+export function NavBar(){
+    return(
+        <nav>
+            <NavLink to="/" >
+            Login {/* Skal ændres til noget andet */}
+            </NavLink>
+            <NavLink to="/addNewModel">
+                AddNewModel
+            </NavLink>
+        </nav>
+    );
+}
